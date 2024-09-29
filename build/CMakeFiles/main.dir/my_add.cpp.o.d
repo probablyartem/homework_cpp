611b4ae94586f13bfbbbfb99ccb65caf17d310d2
Artem <polevartem00@gmail.com>
2024-09-29 20:46:20 +0300

    rebuild repo

--- build/CMakeFiles/main.dir/my_add.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/my_add.cpp.o: \
- /home/artem/Documents/homework_project/my_add.cpp \
- /usr/include/stdc-predef.h
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artem/Documents/homework_project
-# Build directory: /home/artem/Documents/homework_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
